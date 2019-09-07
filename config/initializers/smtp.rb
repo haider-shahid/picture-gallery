785056e7dc1f7d0746ed2031c4b13cba422ffd0a
Haider <konnect2haider@gmail.com>
2019-09-07 23:04:45 +0500

    Production removed

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'haider-gallery.herokuapp.com',
-    user_name: ENV['haider_shahid'],
-    password: ENV['green@grass1'],
-    authentication: :login,
-    enable_starttls_auto: true
-}
